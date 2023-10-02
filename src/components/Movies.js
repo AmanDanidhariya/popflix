5a39c302fe8af048e33da7b61353b58091a80764
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-10-02 19:07:55 +0530

    api calling

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useMovie } from '../context/context'
 
 const Movies = () => {
+  //get movie data from context  
+const {movies} = useMovie();
+  console.log(movies);
   return (
-    <div>Movies</div>
+    <>
+      {movies.map((movie)=>{
+        return (
+          <>
+            <h2>{movie.original_title}</h2>
+          </>
+        )
+      })}
+    </>
   )
 }
 
--- src/context/context.js
+++ src/context/context.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
-const context = () => {
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=2147d6597ab11e70b1c0fc5f5e85528f`;
+
+const MovieContext = createContext();
+
+const MovieProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState({ showError: "false", errorMessage: "" });
+
+  const getMovies = async (url) => {
+    try {
+      //loading indicator value true
+      setIsLoading(true);
+      const response = await axios.get(url);
+      const data = response.data.results;
+      console.log(data);
+      //if we have data then update in movies
+      if (data) {
+        //loading indicator value false
+        setIsLoading(false);
+        //update data in movies array
+        setMovies(data);
+      } else {
+        setError({
+          showError: true,
+          errorMessage: data.error,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies(API_URL);
+  }, []);
+
   return (
-    <div>context</div>
-  )
-}
+    <MovieContext.Provider value={{ isLoading, error, movies }}>
+      {children}
+    </MovieContext.Provider>
+  );
+};
 
-export default context
+//custom hook
+const useMovie = () => {
+  return useContext(MovieContext);
+};
+
+export { MovieContext, MovieProvider, useMovie };
