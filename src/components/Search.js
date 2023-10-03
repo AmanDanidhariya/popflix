40c5652d40e23e690df5653f66f1095bfc15c136
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-10-03 12:30:23 +0530

    make query workable

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useMovie } from "../context/context";
+import styles from "./Search.module.css"
 
 const Search = () => {
+  const { query, setQuery, error } = useMovie();
   return (
-    <div>Search</div>
-  )
-}
+    <div className={styles.search_container}>
+      <form action="#" onSubmit={(e) => e.preventDefault()}>
+        <div className="search_div">
+          <input type="text" placeholder="search here" value={query} onChange={(e)=>setQuery(e.target.value)} className={styles.input_container}/>
+        </div>
+      </form>
+      <div className="movie_card_error">
+        <p>{error.show && error.errorMessage}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
--- src/context/context.js
+++ src/context/context.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`;
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}`;
 
 const MovieContext = createContext();
 
@@ -9,6 +10,7 @@ const MovieProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState({ showError: "false", errorMessage: "" });
+  const [query , setQuery] = useState("")
 
   const getMovies = async (url) => {
     try {
@@ -35,11 +37,14 @@ const MovieProvider = ({ children }) => {
   };
 
   useEffect(() => {
+    if(query){
+      getMovies(`${SEARCH_URL}&query=${query}`);
+    }
     getMovies(API_URL);
-  }, []);
+  }, [query]);
 
   return (
-    <MovieContext.Provider value={{ isLoading, error, movies }}>
+    <MovieContext.Provider value={{ isLoading, error, movies, query , setQuery }}>
       {children}
     </MovieContext.Provider>
   );
